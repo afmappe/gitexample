577d85f744f888977e77bdbd371be56a270d1b12
Andrés Felipe Mape <afmappe@hotmail.com>
2021-07-16 16:50:46 -0500

    Date added

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -6,6 +6,10 @@ namespace ClassLibrary
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; }  
+
+        public DateTime Date { get; set; }
+
+    
     }
 }
